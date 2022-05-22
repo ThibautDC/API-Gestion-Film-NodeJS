870297b0514837f59951f0a3bd3a8c36de706e60
ThibautDC <61777705+ThibautDC@users.noreply.github.com>
2022-05-22 22:19:06 +0200

    Mongoose + fastify
    Debut de transition vers Mongoose et MongoDB.
    Remplacement d'Express par Fastify.

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # TP NODE PROJET C
 ## Comment lancer le projet
 -	Faire `npm i` dans le terminal
--	Puis `node .` dans le terminal
+-	Puis `npm start` dans le terminal pour lancer le projet
 
 ##	Description du projet
 
 Mon projet a pour but de faire une API de gestion de film. Elle gérera le nom des films et leurs descriptions, les acteurs, la date de sortie et le nombre d'entré en salles.
 
 On pourra :
+- Afficher tout les films
+- Chercher un film grace a son nom
 - Ajouter un film
 - Supprimer un film
 - Modifier le nombre d'entrée
--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-const express = require('express')
-const app = express()
+const fastify = require('fastify');
+const app = fastify();
 const films = require('./films.json')
+const mongoose = require('mongoose');
+const { ReactionCollector } = require('discord.js');
 
-// Middleware
-app.use(express.json())
+
+mongoose.connect('mongodb+srv://Thibaut:Thibaut1@cluster0.rji1t.mongodb.net/tpnode?retryWrites=true&w=majority') 
+
+
+const filmSchema = {
+    id:{},
+    name: { type: String, required: true },
+    desc: {type:String, required:true},
+    acteurs:{ type:String, required:true},
+    nbIn:{type:Number, required:true}
+};
+
+const Film = mongoose.model('Film', filmSchema);
 
 //Recupére tout les films
-app.get('/film', (req,res) => {
+app.get('/film', async (req,res) => {
+    let films = await Film.find({});
     res.status(200).json(films)
 })
 
 //Recupére le film avec l'id choisis
-app.get('/film/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    const film = films.find(film => film.id === id)
-    res.status(200).json(film)
+app.get('/film/:id', async (req,res) => {
+//Chercher par name ou _id
+    let films = await Film.findById(req.params.id)
+    res.status(200).json(films)
+
 })
 
 //Ajoute un film
@@ -35,22 +50,31 @@ app.patch('/film/:id', (req,res) => {
 })
 
 //Supprime le film choisis
-app.delete('/film/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-    const film = films.find(film => film.id === id)
-    films.splice(films.indexOf(film),1)
-    res.status(200).json(films)
+app.delete('/film/:name', async (req,res) => {
+    // const id = parseInt(req.params.id)
+    // const film = films.find(film => film.id === id)
+    // films.splice(films.indexOf(film),1)
+    let films = await Film.find({})
+    
+    let filmName = films.filter(f=>f.name==req.params.name)
+    if (filmName.length){
+        await Film.findByIdAndDelete(filmName[0]._id)
+        res.status(200).json(films)
+    }else{
+        res.send("Le film n'existe pas")
+    }
 })
 
+
 //Modif nombre d'entrées
-app.patch('/films/nbin/:id'),(req,res)=>{
-    const id = parseInt(req.params.id)
-    const film = films.find(film => film.id === id)
-    film.nbIn =req.body.nbIn,
-    res.status(200).json(film)
-}
+// app.patch('/films/nbin/:id'),(req,res)=>{
+//     const id = parseInt(req.params.id)
+//     const film = films.find(film => film.id === id)
+//     film.nbIn =req.body.nbIn,
+//     res.status(200).json(film)
+// }
 
 
 app.listen(3000, () => {
-    console.log("Server ON")
+    console.log("Server running")
   })
